47f9e4b595e979344b2b5624fdea512597881517
Josh Hamill <joshua@transposit.com>
2023-06-22 12:27:40 -0400

    rename rover

--- formula/rover-cli.rb
+++ formula/rover-cli.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-class Rover < Formula
+class RoverCli < Formula
   homepage "https://github.com/jhamill34/rover"
   url "https://github.com/jhamill34/rover/releases/download/v0.1.0/rover-v0.1.0-aarch64-apple-darwin.tar.gz"
   sha256 "4a999dc6a2bc91949bd3bf066cd35e7fb953064f719f9fb149dcae52a44771b7"
