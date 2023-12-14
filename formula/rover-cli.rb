fa6f6749621a4c7626611299c2df1061c71860a3
Josh Hamill <joshua@transposit.com>
2023-12-14 16:01:52 -0500

    feat: upgrade rover-cli

--- formula/rover-cli.rb
+++ formula/rover-cli.rb
@@ -1,11 +1,21 @@
 require "formula"
 
+VERSION = "0.1.2"
+
 class RoverCli < Formula
   homepage "https://github.com/jhamill34/rover"
-  url "https://github.com/jhamill34/rover/releases/download/v0.1.0/rover-v0.1.0-aarch64-apple-darwin.tar.gz"
-  sha256 "4a999dc6a2bc91949bd3bf066cd35e7fb953064f719f9fb149dcae52a44771b7"
+  on_macos do
+    on_arm do
+      url "https://github.com/jhamill34/rover/releases/download/v#{VERSION}/rover-v#{VERSION}-aarch64-apple-darwin.tar.gz"
+    end
+
+    on_intel do
+      url "https://github.com/jhamill34/rover/releases/download/v#{VERSION}/rover-v#{VERSION}-x86_64-apple-darwin.tar.gz"
+    end
+  end
+
   head "https://github.com/jhamill34/rover.git"
-  version "0.1.0"
+  version VERSION
 
   def install
     bin.install "rover"
