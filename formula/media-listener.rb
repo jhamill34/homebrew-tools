0a836dca4e39a62c952605c28de5a99c306df710
Josh Hamill <joshrasmussen34@gmail.com>
2026-01-21 17:14:34 -0500

    [jj-spr] initial version
    Created using jj-spr 0.1.0

---
+++ formula/media-listener.rb
@@ -0,0 +1,64 @@
+
+class MediaListener < Formula
+  desc "macOS system-wide media playback monitor with UNIX socket API"
+  homepage "https://github.com/jhamill34/media_listener"
+  head "https://github.com/jhamill34/media_listener.git", branch: "main"
+
+  depends_on :macos
+
+  def install
+    # Create headers directory in build
+    (buildpath/"headers").mkpath
+
+    # Copy headers
+    cp "Sources/media_listener/MediaRemote.h", "headers/"
+    cp "Sources/media_listener/BridgingHeader.h", "headers/"
+
+    # Build using Makefile
+    system "make", "clean"
+    system "make"
+
+    # Install binary
+    bin.install "media_listener"
+
+    # Install headers for reference
+    (prefix/"include").install Dir["headers/*"]
+  end
+
+  def caveats
+    <<~EOS
+      media_listener monitors system-wide media playback on macOS.
+
+      Socket API:
+        Path: /tmp/media_listener.sock
+        Format: JSON (newline-delimited)
+
+      To start the service:
+        brew services start media-listener
+
+      To connect and view events:
+        nc -U /tmp/media_listener.sock
+
+      The service publishes these event types:
+        - now_playing_info_changed: Track changes and playback events
+        - application_changed: Active media app switched
+        - current_state: Initial state on client connect
+
+      For integration examples, see:
+        https://github.com/yourusername/media_listener
+    EOS
+  end
+
+  service do
+    run [opt_bin/"media_listener"]
+    keep_alive true
+    working_dir var
+    log_path var/"log/media_listener.log"
+    error_log_path var/"log/media_listener.error.log"
+  end
+
+  test do
+    assert_predicate bin/"media_listener", :exist?
+    assert_predicate bin/"media_listener", :executable?
+  end
+end
---
+++ formula/sketchybar-media-listener.rb
@@ -0,0 +1,55 @@
+class SketchybarMediaListener < Formula
+  desc "SketchyBar integration for media_listener"
+  homepage "https://github.com/jhamill34/media_listener"
+  head "https://github.com/jhamill34/media_listener.git", branch: "main"
+
+  depends_on "media-listener"
+  depends_on "jq"
+  depends_on "sketchybar"
+
+  def install
+    # Install the sketchybar service script
+    bin.install "sketchybar_media_listener_service.sh" => "sketchybar-media-listener"
+  end
+
+  def caveats
+    <<~EOS
+      SketchyBar media listener integration installed.
+
+      This service connects to media_listener's UNIX socket and triggers
+      SketchyBar updates with media information.
+
+      Setup:
+        1. Make sure media-listener is running:
+           brew services start media-listener
+
+        2. Start this service:
+           brew services start sketchybar-media-listener
+
+        3. Configure your SketchyBar plugin to handle the 'custom_media_change' event:
+           sketchybar --add event custom_media_change
+
+      The service triggers SketchyBar with:
+        sketchybar --trigger custom_media_change MEDIA_INFO="<json>"
+
+      View logs:
+        tail -f #{var}/log/sketchybar_media_listener.log
+
+      The service will automatically reconnect if media_listener restarts.
+    EOS
+  end
+
+  service do
+    run [opt_bin/"sketchybar-media-listener"]
+    keep_alive true
+    working_dir var
+    log_path var/"log/sketchybar_media_listener.log"
+    error_log_path var/"log/sketchybar_media_listener.error.log"
+    environment_variables PATH: std_service_path_env
+  end
+
+  test do
+    assert_predicate bin/"sketchybar-media-listener", :exist?
+    assert_predicate bin/"sketchybar-media-listener", :executable?
+  end
+end
